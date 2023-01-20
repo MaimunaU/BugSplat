3d92417cc4d7a078a2da81ff13d983f4b595f197
MaimunaU <99210737+MaimunaU@users.noreply.github.com>
2023-01-19 22:54:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,199 @@
+pleading = ["Please spare us!", "We beg for mercy!", "We'll leave. Let us go please.", "We won't do any harm.", "We'll never come back, please don't kill us!", "Nooooo! We don't want to die.", "We have families to get back to, spare us please.", "Let us live."];
+nasty = ["We'll take over your world!", "You can't stop us!", "You ugly human!", "My comrades will win, you disgusting rat.", "We hate you.", "We are superior.", "A few deaths will not stop us!", "We'll destroy you all!"];
+messageList = [];
+
+function initialize() {
+    bugs = document.getElementById("bugs");
+    message = document.getElementById("message");
+    bugsNum = 15;
+    i = 8;
+    j = 8;
+}
+
+function changeImage() { 
+    if (document.getElementById("bug1").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug1").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage2() {
+    if (document.getElementById("bug2").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug2").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage3() {
+    if (document.getElementById("bug3").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug3").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage4() {
+    if (document.getElementById("bug4").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug4").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage5() {
+    if (document.getElementById("bug5").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug5").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage6() {
+    if (document.getElementById("bug6").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug6").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage7() {
+    if (document.getElementById("bug7").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug7").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage8() {
+    if (document.getElementById("bug8").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug8").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage9() {
+    if (document.getElementById("bug9").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug9").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage10() {
+    if (document.getElementById("bug10").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug10").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage11() {
+    if (document.getElementById("bug11").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug11").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage12() {
+    if (document.getElementById("bug12").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug12").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage13() {
+    if (document.getElementById("bug13").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug13").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage14() {
+    if (document.getElementById("bug14").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug14").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function changeImage15() {
+    if (document.getElementById("bug15").src = "/images/bug.jpg")
+    {
+        document.getElementById("bug15").src = "/images/splatter.jpg";
+        if (bugsNum > 0) {
+            bugsNum--;
+        }
+        messages();
+    }
+}
+
+function messages() {
+    if (bugsNum > 0) {
+        num = Math.floor(Math.random() * i);
+        num2 = Math.floor(Math.random() * j);
+        if (bugsNum >= 8) {
+            messageList.push(nasty[num] + "<br>");
+            nasty.splice(num, 1);
+            i--;
+        }
+        else {
+            messageList.push(pleading[num2] + "<br>");
+            pleading.splice(num2, 1)
+            j--;
+        }
+    }
+
+    if (bugsNum === 0) {
+        messageList.push("You've splatted us all.<br>")
+    }
+    
+    message.innerHTML = messageList;
+}
